bc4f51c2c777226eeaa900b998d5d92785b5feb9
Luiz Rodrigo Schuitek <rodrigoschuitek@gmail.com>
2019-09-30 23:35:47 -0300

    Criado rota dos arquivos.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+// Publicas
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 // Adicionar validação do token
 routes.use(authMiddleware);
+
+// Usuário
 routes.put('/users', UserController.update);
+
+// Arquivo
+routes.post('/files', upload.single('file'), FileController.store);
+
 
 export default routes;
